a9dd7aeda93b7f06b34dd3010a3dc36af2be9009
Aditya Mehra <aix.m@outlook.com>
2022-09-22 11:24:09 +0930

    change how kernel is found in sj201

--- docker_overlay/overlays/03_sj201/setup_sj201.sh
+++ docker_overlay/overlays/03_sj201/setup_sj201.sh
@@ -12,17 +12,34 @@ pacman --noconfirm -Syyuu
 pacman --noconfirm -Syu python-pip i2c-tools pulseaudio pulseaudio-zeroconf alsa-utils git
 CFLAGS="-fcommon" pip install smbus smbus2 spidev rpi.gpio
 
+# Determine kernel with build directory
+if [ "$(ls -1 /lib/modules | wc -l)" -gt 1 ]; then
+    kernels=($(ls /lib/modules))
+    echo "Looking for kernel with build dir in ${kernels[*]}"
+    for k in "${kernels[@]}"; do
+        if [ -d "/lib/modules/${k}/build" ]; then
+            kernel="${k}"
+            echo "Selected kernel ${kernel}"
+            break
+        fi
+    done
+    if [ -z ${kernel} ]; then
+        echo "No build files available. Picking kernel=${kernels[0]}"
+        kernel=${kernels[0]}
+    fi
+else
+    kernel=$(ls /lib/modules)
+    echo "Only one kernel available: ${kernel}"
+fi
+
 # Build and load VocalFusion Driver
 git clone https://github.com/OpenVoiceOS/vocalfusiondriver
 cd vocalfusiondriver/driver || exit 10
-
-ls /lib/modules/5.15.56-1-MANJARO-ARM-RPI/
-
-sed -ie "s|\$(shell uname -r)|5.15.56-1-MANJARO-ARM-RPI|g" Makefile
+sed -ie "s|\$(shell uname -r)|${kernel}|g" Makefile
 make all || exit 2
-mkdir "/lib/modules/5.15.56-1-MANJARO-ARM-RPI/kernel/drivers/vocalfusion"
-cp vocalfusion* "/lib/modules/5.15.56-1-MANJARO-ARM-RPI/kernel/drivers/vocalfusion" || exit 2
-depmod 5.15.56-1-MANJARO-ARM-RPI -a
+mkdir "/lib/modules/${kernel}/kernel/drivers/vocalfusion"
+cp vocalfusion* "/lib/modules/${kernel}/kernel/drivers/vocalfusion" || exit 2
+depmod ${kernel} -a
 # `modinfo -k ${kernel} vocalfusion-soundcard` should show the module info now
 
 # Configure pulse user
