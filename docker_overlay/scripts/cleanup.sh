abd11c854fb50488186ca35eab0724f6939ed5a9
Aditya Mehra <aix.m@outlook.com>
2022-09-23 09:03:31 +0930

    unmount forcefully

--- docker_overlay/scripts/cleanup.sh
+++ docker_overlay/scripts/cleanup.sh
@@ -6,7 +6,6 @@ cd "${build_dir}" || exit 10
 
 lsof mnt
 sudo umount mnt/etc/resolv.conf || exit 10
-killall gpg-agent
 
 if [ -f mnt/etc/.resolv.conf ]; then
     sudo mv mnt/etc/.resolv.conf mnt/etc/resolv.conf
@@ -17,11 +16,11 @@ sudo mv mnt/tmp/bashrc-cleanup mnt/root/.bashrc
 sudo mv mnt/tmp/cli_login.sh mnt/root/cli_login.sh
 sudo rm -rf mnt/tmp/*
 echo "Temporary files removed"
+sudo umount mnt/boot || exit 10
 sudo umount mnt/run/systemd/resolve || exit 10
 sudo umount mnt/sys || exit 10
 sudo umount mnt/proc || exit 10
-lsof mnt/dev
+fuser -km mnt/dev
 sudo umount mnt/dev || exit 10
-sudo umount mnt/boot || exit 10
 sudo umount mnt || exit 10
 echo "Image unmounted"
