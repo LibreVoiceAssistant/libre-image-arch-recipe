68107d96d7f0bbc622c4841afa89ab1d8147fd6e
Aditya Mehra <aix.m@outlook.com>
2022-09-22 19:29:53 +0930

    try a different method to losetup

--- docker_overlay/scripts/prepare.sh
+++ docker_overlay/scripts/prepare.sh
@@ -17,30 +17,61 @@ recipe_dir="/overlays"
 cd "${build_dir}" || exit 10
 
 # mkdir boot
-# mkdir mnt
-
 mkdir mnt
 
-loop=$(sudo losetup -fP --show "${image_file}")
-echo "Display loop info"
-echo ${loop}
-sleep 3
-boot_part="${loop}p1"
-root_part="${loop}p2"
-
-echo "Mounting ROOT PART"
-sudo mount "${root_part}" mnt
-echo "Mounted ROOT PART"
-sleep 3
-echo "Mounting BOOT PART"
-sudo mount "${boot_part}" mnt/boot/
-echo "Mounted BOOT PART"
-
-echo "Mounted Image FS"
-echo "------------------------------------"
-echo "Copying Boot Overlay Files"
+# mkdir mnt
+#
+# loop=$(sudo losetup -fP --show "${image_file}")
+# echo "Display loop info"
+# echo ${loop}
+# sleep 3
+# boot_part="${loop}p1"
+# root_part="${loop}p2"
+#
+# echo "Mounting ROOT PART"
+# sudo mount "${root_part}" mnt
+# echo "Mounted ROOT PART"
+# sleep 3
+# echo "Mounting BOOT PART"
+# sudo mount "${boot_part}" mnt/boot/
+# echo "Mounted BOOT PART"
+#
+# echo "Mounted Image FS"
+# echo "------------------------------------"
+# echo "Copying Boot Overlay Files"
 # Manjaro Minimal=32000000
-#sudo mount -o loop,offset=32000000 "${image_file}" boot || exit 10
+
+# sudo mount -o loop,offset=32000000 "${image_file}" mnt/boot || exit 10
+
+# if [ "${4}" == "respeaker" ]; then
+#    echo "Selected respeaker boot overlay"
+#    sudo cp -r ${recipe_dir}/00_boot_overlay_respeaker/overlays/* mnt/boot/overlays/
+#    sudo cp -r ${recipe_dir}/00_boot_overlay_respeaker/config.txt mnt/boot/config.txt
+#fi
+#if [ "${4}" == "mark2" ]; then
+#    echo "Selected mark2 boot overlay"
+#    sudo cp -r ${recipe_dir}/00_boot_overlay_mark2/overlays/* mnt/boot/overlays/
+#    sudo cp -r ${recipe_dir}/00_boot_overlay_mark2/config.txt mnt/boot/config.txt
+#fi
+#sleep 2  # Avoid busy target issues
+
+# sudo umount boot
+# rm -r boot
+echo "Boot Files Configured"
+
+echo "Mounting Image FS"
+#Manjaro Minimal=512000512
+sudo mount -o loop,offset=512000512 "${image_file}" mnt && echo "Mounted root image FS" || exit 10
+sudo mkdir -p mnt/run/systemd/resolve
+sudo mount --bind /run/systemd/resolve mnt/run/systemd/resolve  && echo "Mounted resolve directory from host" || exit 10
+sudo mount --bind /etc/resolv.conf mnt/etc/resolv.conf
+sudo mount -t sysfs sys mnt/sys
+sudo mount -t proc proc mnt/proc
+sudo mount -o bind /dev mnt/dev
+
+sleep 3
+
+sudo mount -o loop,offset=32000000 "${image_file}" mnt/boot/ || exit 10
 
 if [ "${4}" == "respeaker" ]; then
     echo "Selected respeaker boot overlay"
@@ -52,28 +83,15 @@ if [ "${4}" == "mark2" ]; then
     sudo cp -r ${recipe_dir}/00_boot_overlay_mark2/overlays/* mnt/boot/overlays/
     sudo cp -r ${recipe_dir}/00_boot_overlay_mark2/config.txt mnt/boot/config.txt
 fi
-sleep 1  # Avoid busy target issues
+sleep 2
 
-#sudo umount boot
-#rm -r boot
-echo "Boot Files Configured"
+ls mnt
+ls mnt/boot/
 
 # Backup original resolv
 if [ -f mnt/etc/resolv.conf ]; then
     sudo mv mnt/etc/resolv.conf mnt/etc/.resolv.conf
 fi
-
-echo "Mounting Image FS"
-# Manjaro Minimal=512000512
-# sudo mount -o loop,offset=512000512 "${image_file}" mnt && echo "Mounted root image FS" || exit 10
-ls mnt
-ls mnt/boot/
-sudo mkdir -p mnt/run/systemd/resolve
-sudo mount --bind /run/systemd/resolve mnt/run/systemd/resolve  && echo "Mounted resolve directory from host" || exit 10
-sudo mount --bind /etc/resolv.conf mnt/etc/resolv.conf
-sudo mount -t sysfs sys mnt/sys
-sudo mount -t proc proc mnt/proc
-sudo mount -o bind /dev mnt/dev
 
 echo "Writing Build Info to Image"
 sudo mkdir -p mnt/opt/ovos
